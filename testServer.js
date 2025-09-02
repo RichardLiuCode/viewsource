24e1bf737d23f5ad4cea70eea9c73db8f331bb6d
Richard Liu <richardadesigner@gmail.com>
2025-09-01 17:54:06 -0700

    update

--- testServer.js
+++ testServer.js
@@ -1,11 +1,10 @@
 window.onload = function () {
-    for (var requestTime = 0; requestTime <= 4; requestTime++)
-        fetch("https://view-source-api.onrender.com/api?site=about:blank")
-            .then(function () {
-                console.log("Server is running!");
-            })
-            .catch(function (error) {
-                console.error("Server is not running!");
-                console.error(error);
-            });
+    fetch("https://view-source-api.onrender.com/api?site=about:blank")
+        .then(function () {
+            console.log("Server is running!");
+        })
+        .catch(function (error) {
+            console.error("Server is not running!");
+            console.error(error);
+        });
 }
